2c98734a3fbb1e02ca56d6a1868315e9f96f2603
Pär Thernström <par.thernstrom@gmail.com>
2018-09-01 19:04:30 +0200

    Add GA

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactGA from 'react-ga';
 
 import Header from "./header.js";
 import Footer from "./footer.js";
@@ -10,6 +11,11 @@ import "./header.css";
 import "./signup.css";
 
 class App extends Component {
+  componentDidMount() {
+    ReactGA.initialize('UA-181460-40');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+  
   render() {
     return (
       <div className="App">
